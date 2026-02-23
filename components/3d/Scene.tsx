a089585f73a638bf650c014a5761a949f34909ba
Web3kaneco <john@lazylions.co>
2026-02-22 21:03:07 -0500

    Update Scene.tsx

--- components/3d/Scene.tsx
+++ components/3d/Scene.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Environment } from '@react-three/drei';
 import { Suspense } from 'react';
-import { Model as Avatar } from './AvatarModel';
+import { Canvas } from '@react-three/fiber';
+import { Environment, OrbitControls } from '@react-three/drei';
+import { Avatar } from './Avatar'; 
 
+// 1. Give Scene its own guest list so it can accept the data from page.tsx!
 interface SceneProps {
   modelUrl: string;
   volumeRef: React.MutableRefObject<number>;
@@ -13,10 +14,9 @@ interface SceneProps {
 export default function Scene({ modelUrl, volumeRef }: SceneProps) {
   return (
     <Canvas camera={{ position: [0, 1.5, 4], fov: 50 }}>
-      <ambientLight intensity={0.6} />
-      <directionalLight position={[5, 5, 5]} intensity={1.5} castShadow />
       <Environment preset="city" />
       <Suspense fallback={null}>
+         {/* 2. Pass the data straight down into your flawless Avatar component */}
          <Avatar modelUrl={modelUrl} volumeRef={volumeRef} />
       </Suspense>
       <OrbitControls enableZoom={false} enablePan={false} maxPolarAngle={Math.PI / 2 + 0.1} minPolarAngle={Math.PI / 3} />
